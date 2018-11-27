f14d7578dc4e66b668691bc3a10c5d33fc9b018e
flatko84 <flatko@gmail.com>
2018-11-27 15:24:57 +0200

    Qwerty added, Back To Stats button added

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Hang Me</div>
+                <div class="card-header">Hang Me<div><input type=button value="Back To Stats" onClick="window.location='/home'" style="display: inline-block; position: relative; right:0"></div></div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -19,32 +19,32 @@
 
 
                       <img id="pic" src="0.png"><br>
-                    <input type="button" class="letter" value="a" id="a">
-                    <input type="button" class="letter" value="b" id="b">
-                    <input type="button" class="letter" value="c" id="c">
-                    <input type="button" class="letter" value="d" id="d">
+                    <input type="button" class="letter" value="q" id="q">
+                    <input type="button" class="letter" value="w" id="w">
                     <input type="button" class="letter" value="e" id="e">
+                    <input type="button" class="letter" value="r" id="r">
+                    <input type="button" class="letter" value="t" id="t">
+                    <input type="button" class="letter" value="y" id="y">
+                    <input type="button" class="letter" value="u" id="u">
+                    <input type="button" class="letter" value="i" id="i">
+                    <input type="button" class="letter" value="o" id="o">
+                    <input type="button" class="letter" value="p" id="p"><br>
+                    &nbsp;&nbsp;&nbsp;<input type="button" class="letter" value="a" id="a">
+                    <input type="button" class="letter" value="s" id="s">
+                    <input type="button" class="letter" value="d" id="d">
                     <input type="button" class="letter" value="f" id="f">
                     <input type="button" class="letter" value="g" id="g">
                     <input type="button" class="letter" value="h" id="h">
-                    <input type="button" class="letter" value="i" id="i">
                     <input type="button" class="letter" value="j" id="j">
                     <input type="button" class="letter" value="k" id="k">
-                    <input type="button" class="letter" value="l" id="l">
+                    <input type="button" class="letter" value="l" id="l"><br>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="button" class="letter" value="z" id="z">
+                    <input type="button" class="letter" value="x" id="x">
+                    <input type="button" class="letter" value="c" id="c">
+                    <input type="button" class="letter" value="v" id="v">
+                    <input type="button" class="letter" value="b" id="b">
                     <input type="button" class="letter" value="m" id="m">
                     <input type="button" class="letter" value="n" id="n">
-                    <input type="button" class="letter" value="o" id="o">
-                    <input type="button" class="letter" value="p" id="p">
-                    <input type="button" class="letter" value="q" id="q">
-                    <input type="button" class="letter" value="r" id="r">
-                    <input type="button" class="letter" value="s" id="s">
-                    <input type="button" class="letter" value="t" id="t">
-                    <input type="button" class="letter" value="u" id="u">
-                    <input type="button" class="letter" value="v" id="v">
-                    <input type="button" class="letter" value="w" id="w">
-                    <input type="button" class="letter" value="x" id="x">
-                    <input type="button" class="letter" value="y" id="y">
-                    <input type="button" class="letter" value="z" id="z">
 
                   <div>Whole word: <input id="word" name="whole"></input><input type="button" value="Guess" id="whole"></div>
 
